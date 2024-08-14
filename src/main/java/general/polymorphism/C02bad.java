b7719b4c4c4730c3851945093c7163e57845d1b7
wxie7 <xieym3@zohomail.com>
2024-08-14 23:54:26 +0800

    rename

--- src/main/java/general/polymorphism/C02bad.java
+++ src/main/java/general/polymorphism/C02bad.java
@@ -1,23 +1,22 @@
 package general.polymorphism;
 
 import static defect.InfoLeak.*;
-import static defect.InfoLeak.sink;
 
 /**
- * condition comes from super method, bad case
+ * condition comes from super method, good case
  */
 public class C02bad {
     public static void main(String[] args) {
         B b = new B();
-        b.good();
+        b.bad();
     }
 
     static class A {
-        boolean cond(boolean cond) { return cond; }
+        boolean cond(boolean cond) { return !cond; }
     }
 
     static class B extends A {
-        public void good() {
+        public void bad() {
             boolean cond = randomBoolean();
             String source = cond ? safe() : source();
             cond = super.cond(cond);
--- src/main/java/general/polymorphism/C02good.java
+++ src/main/java/general/polymorphism/C02good.java
@@ -1,9 +1,10 @@
 package general.polymorphism;
 
 import static defect.InfoLeak.*;
+import static defect.InfoLeak.sink;
 
 /**
- * condition comes from super method, good case
+ * condition comes from super method, bad case
  */
 public class C02good {
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class C02good {
     }
 
     static class A {
-        boolean cond(boolean cond) { return !cond; }
+        boolean cond(boolean cond) { return cond; }
     }
 
     static class B extends A {
